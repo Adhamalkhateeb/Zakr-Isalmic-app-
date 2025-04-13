f8e30e5f9df94bdbff278d343c944d8bd556afbd
Adhamalkhateeb <adhamalkhateeb2006@gmail.com>
2025-04-13 23:28:23 +0200

    Added Sources Needed to run App

---
+++ FileManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Islamic_app
+{
+    public static class FileManager
+    {
+        public static string AppPath => Path.GetDirectoryName(Application.ExecutablePath);
+
+        public static string[] ReadAllLines(string fileName)
+        {
+            string filePath = Path.Combine(AppPath, fileName);
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"File not found: {filePath}");
+            }
+            return File.ReadAllLines(filePath);
+        }
+
+        public static string GetResourcePath(string folder, string fileName)
+        {
+            string directoryPath = Path.Combine(AppPath, folder);
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            return Path.Combine(directoryPath, fileName);
+        }
+
+        public static void ExtractEmbeddedResources()
+        {
+            string[] folders = { "Surahs", "Data" };
+
+            foreach (var folder in folders)
+            {
+                string folderPath = Path.Combine(AppPath, folder);
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+            }
+        }
+
+        public static bool ResourceExists(string folder, string fileName)
+        {
+            string filePath = Path.Combine(AppPath, folder, fileName);
+            return File.Exists(filePath);
+        }
+    }
+}
